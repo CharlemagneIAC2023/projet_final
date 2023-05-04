a924e9e02cd6a41e80b9d432bcfccfacae5bda52
Charlemagne <clement.charlemagne@ynov.com>
2023-05-04 10:42:19 +0200

    maj fichiers scripts

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -2,9 +2,12 @@
 
 
 rm -rfv ../carolus_ia_final/
-rm -rfv ../../fetch_data_dag.py
+rm -fv ../../fetch_data_dag.py
 rm -rfv ../../__pycache__/
-rm -rfv ../../train_model.py
+rm -fv ../../train_model.py
 rm -rfv ../dev/models/
 rm -rfv ../dev/__pycache__/
-rm -rfv ../dev/weather_data.db
+rm -fv ../dev/weather_data.db
+
+rm -rfv models/
+rm -fv weather_data.db
